aa70eae4aa143666fdfe88e05232a814abdf11ae
kaolnwza <kaow_jijuy999@hotmail.com>
2019-11-04 14:54:41 +0700

    Update process.js

--- process.js
+++ process.js
@@ -226,3 +226,84 @@ function arithmetic_geometic(){
         document.getElementById("geometic_ans").innerHTML = ans
     }
 }
+function static() {
+    var input;
+    var med = 0,
+        mode,
+        xbar = 0,
+        sam = 0;
+    var input_notdone,
+        input_done = [];
+    var sd_sigma,
+        sd_saved = 0,
+        sd_s,
+        sd_up2;
+
+    // part of input
+    input = document.forms["input_form"]["inputval"].value
+        .split(" ")
+        .filter(val => val != "")
+        .map(Number);
+
+    for (i = 0; i < input.length; i++) {
+        if (!isNaN(input[i])) {
+            input_done.push(input[i]);
+        }
+    }
+    // end of input part
+
+    // xbar
+    sam = 0;
+    xbar = 0;
+    for (i = 0; i < input_done.length; i++) {
+        sam = sam + input_done[i];
+    }
+    xbar = sam / input_done.length;
+    // end of xbar
+
+    // median
+    if (input_done.length % 2 !== 0) {
+        med = input_done[Math.floor(input_done.length / 2)];
+    } else if (input_done.length % 2 === 0) {
+        med =
+            (input_done[Math.floor(input_done.length / 2) - 1] +
+                input_done[Math.floor(input_done.length / 2)]) /
+            2;
+    }
+    // end of median
+
+    // S.D.(sigma)
+    for (i = 0; i < input_done.length; i++) {
+        sd_saved = sd_saved + (input_done[i] - xbar) ** 2;
+    }
+    sd_sigma = (sd_saved / input_done.length) ** 0.5;
+    // end of S.D.(sigma)
+
+    // S.D.(s)
+    sd_s = (sd_saved / (input_done.length - 1)) ** 0.5;
+    // end of S.D.(s)
+
+    // S.D. up 2
+    sd_up2 = sd_saved / input_done.length;
+    // end of S.D. up 2
+
+    //Mode
+
+    document.getElementById("xbar_id").value = xbar;
+    document.getElementById("med_id").value = med;
+    document.getElementById("sd_sigma_id").value = sd_sigma;
+    document.getElementById("sd_s_id").value = sd_s;
+    document.getElementById("sd_up2_id").value = sd_up2;
+    document.getElementById("sorted_id").value = String(
+        input_done.sort()
+    ).split(",");
+    document.getElementById("count_id").value = input_done.length;
+    document.getElementById("sum_id").value = input_done.reduce(
+        (x1, x2) => x1 + x2
+    );
+    document.getElementById("max_val_id").value =
+        input_done[input_done.length - 1];
+    document.getElementById("min_val_id").value = input_done[0];
+    //document.getElementById("mode_id").value = ;
+    document.getElementById("tss").value = test;
+}
